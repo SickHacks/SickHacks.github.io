b1237224894006f96c73a8ccd4644885aeb07ee0
QWE RTY <ferobreque@gmail.com>
2024-09-19 16:00:15 -0300

    ojala funcione

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://SickHacks.github.io/",
   base: "/",
   trailingSlash: "always",
   integrations: [
---
+++ src/content/posts/blog1.md
@@ -0,0 +1,13 @@
+---
+title: blog1
+published: 2024-09-19
+description: 'blog de prueba'
+image: ''
+tags: [test, prueba]
+category: ''
+draft: false 
+lang: ''
+---
+
+
+# Esta es una prueba de blog en markdown
