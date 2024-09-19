729611e20e4c3f3b847ed3dd3312af8d391ec1c3
QWE RTY <ferobreque@gmail.com>
2024-09-19 20:38:13 -0300

    icon changes

--- src/config.ts
+++ src/config.ts
@@ -51,18 +51,18 @@ export const profileConfig: ProfileConfig = {
   name: 'Sick Hacks',
   bio: 'Programming & Cybersecurity',
   links: [
-    {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+   // {
+   //   name: 'Twitter',
+   //   icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
-    },
-    {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
+   //   url: 'https://twitter.com',
+   // },
+   // {
+   //   name: 'Steam',
+   //   icon: 'fa6-brands:steam',
+   //   url: 'https://store.steampowered.com',
+   // },
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
